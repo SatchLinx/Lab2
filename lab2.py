4748ebca366fc1516f4b9a15fccdc7fe59a3fa5f
JunYuan <junyuan02.24@ichat.sp.edu.sg>
2025-05-18 20:31:16 +0800

    Changes to BMI Classification

--- lab2.py
+++ lab2.py
@@ -5,10 +5,13 @@ def calculate_bmi(height,weight):
     print("BMI = " + str(BMI))
     if BMI < 18.5:
         print("Under Weight")
+        return -1
     elif BMI >= 18.5 and BMI <=25.0:
         print("Normal weight")
+        return 0
     else:
         print("Over Weight")
+        return 1
      
 
 
